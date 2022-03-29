5d16279c4dd216ed818fc41c28ea185c9fc69bea
Rai Phairow <raiphairow@Rais-MacBook-Pro.local>
2022-03-28 20:41:57 -0700

    fix camera jitter

---
+++ Assets/Scripts/CameraFollow.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFollow : MonoBehaviour
+{
+
+    private Transform player;
+
+    private Vector3 tempPos;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        player = GameObject.FindWithTag("Player").transform;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    void LateUpdate() {
+        UpdateCamera();
+    }
+
+    void UpdateCamera() {
+        tempPos = transform.position;
+        tempPos.x = player.position.x;
+
+        transform.position = tempPos;
+    }
+}
