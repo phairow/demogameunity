5dd0d77c590d07644b7ccab0473613bb83b8f1e9
Rai Phairow <raiphairow@Rais-MacBook-Pro.local>
2022-03-28 19:26:34 -0700

    key handling

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -2,8 +2,31 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class walk : MonoBehaviour
+public class Player : MonoBehaviour
 {
+
+    [SerializeField]
+    private float moveForce = 10f;
+
+    [SerializeField]
+    private float jumpForce = 11f;
+
+    private float movementX;
+
+    private Rigidbody2D myBody;
+
+    private SpriteRenderer sr;
+
+    private Animator anim;
+
+    private string WALK_ANIMATION = "Walk";
+
+    void Awake() {
+        myBody = GetComponent<Rigidbody2D>();
+        sr = GetComponent<SpriteRenderer>();
+        anim = GetComponent<Animator>();
+    }
+
     // Start is called before the first frame update
     void Start()
     {
@@ -13,6 +36,28 @@ public class walk : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        PlayerMoveKeyboard();
+        AnimatePlayer();
+    }
+
+    void PlayerMoveKeyboard() {
+        movementX = Input.GetAxis("Horizontal");
+
+        Debug.Log("x value is : " + movementX + " , delta time: " + Time.deltaTime);
+
+        transform.position += new Vector3(movementX, 0f, 0f) * Time.deltaTime * moveForce;
+    }
+
+    void AnimatePlayer() {
+
+        if (movementX > 0 ) {
+            sr.flipX = false;
+            anim.SetBool(WALK_ANIMATION, true);
+        } else if (movementX < 0) {
+            sr.flipX = true;
+            anim.SetBool(WALK_ANIMATION, true);
+        } else {
+            anim.SetBool(WALK_ANIMATION, false);
+        }
     }
 }
